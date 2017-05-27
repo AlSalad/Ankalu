e366ac8cb9362f6d8d222bd8b9657eb89fe9d55a
Ravioli <koopacopter@gmail.com>
2017-05-27 12:01:52 +0200

    testing

---
+++ code/Ankalu/src/Tests/GameTest.java
@@ -0,0 +1,72 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by koopa on 27.05.2017.
+ */
+public class GameTest {
+    @Test
+    public void addPropertyChangeListener() throws Exception {
+
+    }
+
+    @Test
+    public void removePropertyChangeListener() throws Exception {
+    }
+
+    @Test
+    public void getState() throws Exception {
+        int state = 0;
+        assertTrue(state == 0);
+    }
+
+    @Test
+    public void getLevel() throws Exception {
+        int level = 1;
+        assertTrue(level == 1);
+    }
+
+    @Test
+    public void getScore() throws Exception {
+        int score = 1;
+        assertTrue(score == 1);
+    }
+
+    @Test
+    public void getRemovedLines() throws Exception {
+        //SquareBoard board = mock(SquareBoard.class);
+        //assertTrue(board.getRemovedLines() == 0);
+    }
+
+    @Test
+    public void getSquareBoardComponent() throws Exception {
+    }
+
+    @Test
+    public void getPreviewBoardComponent() throws Exception {
+    }
+
+    @Test
+    public void init() throws Exception {
+        int state = 4;
+
+    }
+
+    @Test
+    public void start() throws Exception {
+    }
+
+    @Test
+    public void pause() throws Exception {
+    }
+
+    @Test
+    public void resume() throws Exception {
+    }
+
+    @Test
+    public void terminate() throws Exception {
+    }
+
+}
